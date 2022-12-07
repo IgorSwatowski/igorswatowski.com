22b19cc818235ec7e5eabff6b366a5615d4e12c8
IgorSwatowski <igi66@wp.pl>
2022-12-07 11:14:03 +0100

    Add English and Polish language provider with i18n

---
+++ i18n/locales/en.ts
@@ -0,0 +1,7 @@
+const en = {
+    "hello-world": "Hello, world!",
+    "welcome-message": "Welcome to our website!",
+    "goodbye-message": "Goodbye and thank you for visiting!",
+  };
+  
+  export { en };
