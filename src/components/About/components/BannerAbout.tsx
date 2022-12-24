ad7413f5a498bc591eac1aa4491e7b105e1b02c4
IgorSwatowski <igorswatowski@gmail.com>
2022-12-24 10:43:54 +0100

    Split about page to simpler components

---
+++ src/components/About/components/BannerAbout.tsx
@@ -0,0 +1,56 @@
+import { useRouter } from 'next/router';
+
+import { en } from '../../../i18n/locales/en';
+import { pl } from '../../../i18n/locales/pl';
+
+import BannerMeImg from '../../../assets/about-igor.webp';
+import Image from 'next/image';
+import Button from '../../Button/Button';
+
+const BannerAbout = () => {
+  const router = useRouter();
+  const { locale } = router;
+  const t = locale === 'en' ? en : pl;
+  return (
+    <section className="banner-text">
+      <div className="banner-text-wrapper container-box">
+        <div className="banner-text-wrapper-left">
+          <h1
+            className="banner-text-wrapper-heading heading-primary"
+            data-aos="fade-down"
+            data-aos-delay="50"
+          >
+            {t.bannerHeading}
+          </h1>
+          <p
+            className="banner-text-wrapper-text paragraph-primary"
+            data-aos="fade-down"
+            data-aos-delay="100"
+          >
+            {t.bannerText}
+          </p>
+          <p
+            className="banner-text-wrapper-text paragraph-primary"
+            data-aos="fade-down"
+            data-aos-delay="100"
+          >
+            {t.bannerTextSecond}
+          </p>
+          <p
+            className="banner-text-wrapper-text paragraph-primary"
+            data-aos="fade-down"
+            data-aos-delay="100"
+          >
+            {t.bannerTextThird}
+          </p>
+          <Button href="packages">{t.aboutCtaSecondBtn}</Button>
+        </div>
+        <div className="banner-text-wrapper-right">
+          <Image loading="lazy" src={BannerMeImg} alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BannerAbout;
