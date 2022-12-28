ae1e5c08c666eaf30acd11fec9a33df4701190d6
IgorSwatowski <igorswatowski@gmail.com>
2022-12-28 10:50:40 +0100

    Fix sendgrid

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,2 @@
 export const graphqlAPI = process.env.HYGRAPH_PROJECT_API!;
-export const SENDGRID_API = process.env.SENDGRID_API_KEY!;
+export const SENDGRID_API = process.env.SENDGRID_SECRET!;
--- src/pages/api/sendgrid.ts
+++ src/pages/api/sendgrid.ts
@@ -1,26 +1,64 @@
-import { SENDGRID_API } from '../../constants/constants';
 import sendgrid from '@sendgrid/mail';
+import { SENDGRID_API } from '../../constants/constants';
 
 sendgrid.setApiKey(SENDGRID_API);
 
-export default async function sendMail(req: any, res: any) {
-  // You can get the email address from the request body
-  const { email } = req.body;
-
+async function sendEmail(req: any, res: any) {
   try {
-    // Send the email using the SendGrid API
     await sendgrid.send({
-      to: 'hello@igorswatowski.com', // Replace with the email address you want to send to
-      from: 'hello@igorswatowski.com', // Replace with the email address you want to use as the sender
-      subject: 'Hello from Vercel',
-      html: `<p>Hello,</p><p>This is a test email sent from Vercel.</p>`,
+      to: 'hello@igorswatowski.com', // Your email where you'll receive emails
+      from: 'hello@igorswatowski.com', // your website email address here
+      subject: `[Lead from website] : ${req?.body.topic}`,
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+      
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="The HTML5 Herald">
+        <meta name="author" content="SitePoint">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+      
+      </head>
+      
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
+              </div>
+              <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <h3>You've got a new mail from ${req.body.firstName}, their email is: ✉️${req.body.email} </h3>
+              <h3>${req.body.firstName}</h3>
+              <h3>${req.body.lastName}</h3>
+              <h3>${req.body.email}</h3>
+              <h3>${req.body.topic}</h3>
+              <h3>${req.body.company}</h3>
+              <div style="font-size: 16px;">
+              <p>Message:</p>
+              <p>${req.body.message}</p>
+              <br>
+              </div>
+              <img src="https://manuarora.in/logo.png" class="logo-image" style="height: 50px;width: 50px;border-radius: 5px;overflow: hidden;">
+              <p class="footer" style="font-size: 16px;padding-bottom: 20px;border-bottom: 1px solid #D1D5DB;">Regards<br>Manu Arora<br>Software Developer<br>+91 9587738861</p>
+              <div class="footer-links" style="display: flex;justify-content: center;align-items: center;">
+                <a href="https://manuarora.in/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Website</a>
+                <a href="https://manuarora.in/blog/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Blog</a>
+                <a href="https://github.com/manuarora700/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">GitHub</a>
+                <a href="https://instagram.com/maninthere/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Instagram</a>
+                <a href="https://linkedin.com/in/manuarora28/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">LinkedIn</a>
+                <a href="https://twitter.com/mannupaaji/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Twitter</a>
+                
+              </div>
+              </div>
+      </body>
+      </html>`,
     });
   } catch (error: any) {
-    // If there is an error, log it and return a 500 status code
-    console.error(error);
-    return res.status(500).json({ error: error.message });
+    console.log(error);
+    return res.status(error.statusCode || 500).json({ error: error.message });
   }
 
-  // If the email was sent successfully, return a 200 status code
   return res.status(200).json({ error: '' });
 }
+
+export default sendEmail;
