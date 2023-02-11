1eae06c7a5a844b2914a990214f2f6f34f8fa6eb
IgorSwatowski <igorswatowski@gmail.com>
2023-02-11 12:06:01 +0100

    Optimalization

--- src/components/About/components/BannerAbout.tsx
+++ src/components/About/components/BannerAbout.tsx
@@ -5,7 +5,7 @@ import { pl } from '../../../i18n/locales/pl';
 
 import BannerMeImg from '../../../assets/about-igor.webp';
 import Image from 'next/image';
-import Button from '../../Button/Button';
+import { Button } from '../../Button/Button';
 import Link from 'next/link';
 
 import TriangleColor from '../../../assets/triangle-color.png';
@@ -15,51 +15,51 @@ const BannerAbout = () => {
   const { locale } = router;
   const t = locale === 'en' ? en : pl;
   return (
-    <section className="banner-text">
-      <Image src={TriangleColor} alt="" />
-      <div className="banner-text-wrapper container-box">
-        <div className="banner-text-wrapper-left">
+    <section className='banner-text'>
+      <Image src={TriangleColor} alt='' />
+      <div className='banner-text-wrapper container-box'>
+        <div className='banner-text-wrapper-left'>
           <h1
-            className="banner-text-wrapper-left-heading heading-primary"
-            data-aos="fade-down"
-            data-aos-delay="50"
+            className='banner-text-wrapper-left-heading heading-primary'
+            data-aos='fade-down'
+            data-aos-delay='50'
           >
             {t.bannerHeading}
-            <span className="underline">{t.bannerHeadingSecond}</span>
+            <span className='underline'>{t.bannerHeadingSecond}</span>
           </h1>
           <p
-            className="banner-text-wrapper-left-text paragraph-primary"
-            data-aos="fade-down"
-            data-aos-delay="100"
+            className='banner-text-wrapper-left-text paragraph-primary'
+            data-aos='fade-down'
+            data-aos-delay='100'
           >
             {t.bannerText}
           </p>
           <p
-            className="banner-text-wrapper-left-text paragraph-primary"
-            data-aos="fade-down"
-            data-aos-delay="100"
+            className='banner-text-wrapper-left-text paragraph-primary'
+            data-aos='fade-down'
+            data-aos-delay='100'
           >
             {t.bannerTextSecond}
           </p>
           <p
-            className="banner-text-wrapper-left-text paragraph-primary"
-            data-aos="fade-down"
-            data-aos-delay="100"
+            className='banner-text-wrapper-left-text paragraph-primary'
+            data-aos='fade-down'
+            data-aos-delay='100'
           >
             {t.bannerTextThird}
           </p>
-          <div className="banner-text-wrapper-left-buttons">
-            <Link href="/packages" className="btn-primary">
+          <div className='banner-text-wrapper-left-buttons'>
+            <Link href='/packages' className='btn-primary'>
               <span>{t.aboutCtaSecondBtn}</span>
-              <svg width="13px" height="10px" viewBox="0 0 13 10">
-                <path d="M1,5 L11,5"></path>
-                <polyline points="8 1 12 5 8 9"></polyline>
+              <svg width='13px' height='10px' viewBox='0 0 13 10'>
+                <path d='M1,5 L11,5'></path>
+                <polyline points='8 1 12 5 8 9'></polyline>
               </svg>
             </Link>
           </div>
         </div>
-        <div className="banner-text-wrapper-right">
-          <Image loading="lazy" src={BannerMeImg} alt="" />
+        <div className='banner-text-wrapper-right'>
+          <Image loading='lazy' src={BannerMeImg} alt='' />
         </div>
       </div>
     </section>
--- src/components/About/components/CTAAboutSecondSection.tsx
+++ src/components/About/components/CTAAboutSecondSection.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Button from '../../../components/Button/Button';
+import { Button } from '../../Button/Button';
 
 import ColorfulThreeLinesSvg from '../../../assets/colorful-three-lines.png';
 import { useRouter } from 'next/router';
@@ -12,27 +12,27 @@ const CTAAboutSecondSection = () => {
   const { locale } = router;
   const t = locale === 'en' ? en : pl;
   return (
-    <section className="cta-second-about-me">
-      <div className="cta-second-about-me-wrapper container-box">
-        <div className="cta-second-about-me-wrapper-left">
-          <Image loading="lazy" src={ColorfulThreeLinesSvg} alt="" />
+    <section className='cta-second-about-me'>
+      <div className='cta-second-about-me-wrapper container-box'>
+        <div className='cta-second-about-me-wrapper-left'>
+          <Image loading='lazy' src={ColorfulThreeLinesSvg} alt='' />
         </div>
-        <div className="cta-second-about-me-wrapper-right">
+        <div className='cta-second-about-me-wrapper-right'>
           <h2
-            className="cta-second-about-me-wrapper-right-heading heading-secondary"
-            data-aos="fade-down"
-            data-aos-delay="50"
+            className='cta-second-about-me-wrapper-right-heading heading-secondary'
+            data-aos='fade-down'
+            data-aos-delay='50'
           >
             {t.aboutCtaSecondHeading}
           </h2>
           <p
-            className="cta-second-about-me-wrapper-right-text paragraph-primary"
-            data-aos="fade-down"
-            data-aos-delay="100"
+            className='cta-second-about-me-wrapper-right-text paragraph-primary'
+            data-aos='fade-down'
+            data-aos-delay='100'
           >
             {t.aboutCtaSecondText}
           </p>
-          <Button href="/packages">{t.aboutCtaSecondBtn}</Button>
+          <Button href='/packages'>{t.aboutCtaSecondBtn}</Button>
         </div>
       </div>
     </section>
