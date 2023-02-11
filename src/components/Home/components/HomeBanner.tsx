7d2f55f0ef3acbdabbef3368a961d631b076d19f
IgorSwatowski <igorswatowski@gmail.com>
2023-02-11 11:42:07 +0100

    General fixes

--- src/components/Home/components/HomeBanner.tsx
+++ src/components/Home/components/HomeBanner.tsx
@@ -15,33 +15,58 @@ const HomeBanner = () => {
   const { locale } = router;
   const t = locale === 'en' ? en : pl;
   return (
-    <section className="banner container-box">
-      <div className="banner-block-left" data-aos="fade-up">
-        <h1 className="banner-block-left--title heading-primary">{t.hero}</h1>
-        <p className="banner-block-left--sub paragraph-primary">{t.heroText}</p>
-        <p className="banner-block-left--sub paragraph-primary">{t.heroTextSecond}</p>
-        <div className="banner-block-left-buttons">
-          <Link href="/about" className="banner-block-left-buttons-first btn-primary">
+    <section className='banner container-box'>
+      <div className='banner-block-left' data-aos='fade-up'>
+        <h1 className='banner-block-left--title heading-primary'>{t.hero}</h1>
+        <p className='banner-block-left--sub paragraph-primary'>{t.heroText}</p>
+        <p className='banner-block-left--sub paragraph-primary'>
+          {t.heroTextSecond}
+        </p>
+        <div className='banner-block-left-buttons'>
+          <Link
+            href='/about'
+            className='banner-block-left-buttons-first btn-primary'
+          >
             <span>{t.heroBtn}</span>
-            <svg width="13px" height="10px" viewBox="0 0 13 10">
-              <path d="M1,5 L11,5"></path>
-              <polyline points="8 1 12 5 8 9"></polyline>
+            <svg width='13px' height='10px' viewBox='0 0 13 10'>
+              <path d='M1,5 L11,5'></path>
+              <polyline points='8 1 12 5 8 9'></polyline>
             </svg>
           </Link>
         </div>
-        <div className="banner-block-left-trusted">
-          <span className="banner-block-left-trusted--small">{t.trustedBy}</span>
-          <div className="banner-block-left-trusted-logos">
-            <Image loading="lazy" src={GammasoftLogo} alt="Gammasoft" width={100} height={100} />
-            <Image loading="lazy" src={AionlineLogo} alt="aoinline" width={100} height={100} />
-            <Image loading="lazy" src={CodemaLogo} alt="Codema" width={100} height={100} />
+        <div className='banner-block-left-trusted'>
+          <span className='banner-block-left-trusted--small'>
+            {t.trustedBy}
+          </span>
+          <div className='banner-block-left-trusted-logos'>
+            <Image
+              src={GammasoftLogo}
+              alt='Gammasoft'
+              width={100}
+              height={100}
+              priority
+            />
+            <Image
+              priority
+              src={AionlineLogo}
+              alt='aoinline'
+              width={100}
+              height={100}
+            />
+            <Image
+              priority
+              src={CodemaLogo}
+              alt='Codema'
+              width={100}
+              height={100}
+            />
           </div>
         </div>
       </div>
-      <div className="banner-block-right" data-aos="fade-up">
-        <div className="banner-block-right-img">
-          <Image src={MeImg} alt="Personal photo" />
-          <span className="circle"></span>
+      <div className='banner-block-right' data-aos='fade-up'>
+        <div className='banner-block-right-img'>
+          <Image priority src={MeImg} alt='Personal photo' />
+          <span className='circle'></span>
         </div>
       </div>
     </section>
--- src/components/Home/components/ReferencesSection.tsx
+++ src/components/Home/components/ReferencesSection.tsx
@@ -12,22 +12,31 @@ const ReferencesSection = () => {
   const { locale } = router;
   const t = locale === 'en' ? en : pl;
   return (
-    <section className="references container-box">
-      <div className="references-wrapper">
-        <div className="references-wrapper-left">
-          <h3 className="references-wrapper-left-heading heading-references" data-aos="fade-up">
+    <section className='references container-box'>
+      <div className='references-wrapper'>
+        <div className='references-wrapper-left'>
+          <h3
+            className='references-wrapper-left-heading heading-references'
+            data-aos='fade-up'
+          >
             {t.reference}
           </h3>
-          <div className="references-wrapper-left-author" data-aos="fade-up" data-aos-delay="100">
-            <div className="references-wrapper-left-author-top">
-              <Image loading="lazy" src={QuoteSvg} alt="" />
-              <h4 className="heading-fourth">Agnieszka</h4>
+          <div
+            className='references-wrapper-left-author'
+            data-aos='fade-up'
+            data-aos-delay='100'
+          >
+            <div className='references-wrapper-left-author-top'>
+              <Image loading='lazy' src={QuoteSvg} alt='Quote img' />
+              <h4 className='heading-fourth'>Agnieszka</h4>
             </div>
-            <p className="references-wrapper-left-author-company">CEO | Agavaske AS</p>
+            <p className='references-wrapper-left-author-company'>
+              CEO | Agavaske AS
+            </p>
           </div>
         </div>
-        <div className="references-wrapper-right">
-          <Image loading="lazy" src={ColorfulThreeLinesSvg} alt="" />
+        <div className='references-wrapper-right'>
+          <Image loading='lazy' src={ColorfulThreeLinesSvg} alt='' />
         </div>
       </div>
     </section>
