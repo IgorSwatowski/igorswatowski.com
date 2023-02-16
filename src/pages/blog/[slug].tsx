e17be9eaf99e216fa6dce78aee68d742f9e58518
IgorSwatowski <igorswatowski@gmail.com>
2023-02-16 22:43:11 +0100

    Refactor slug page

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,22 +1,33 @@
-import { client, previewClient } from '../../lib/contentful/client';
+import { client, previewClient } from '@/lib/contentful/client';
 import { useRouter } from 'next/router';
+import { GetStaticProps, GetStaticPaths } from 'next';
 
-const Post = ({ post, preview }: any) => {
+interface PostProps {
+  post: any;
+  preview: boolean;
+}
+
+const Post = ({ post, preview }: PostProps) => {
   const router = useRouter();
+
+  if (router.isFallback || !post) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <section className='section'>
-      {preview && <p>alert</p>}
+      {preview && <p>Preview mode enabled!</p>}
       <div className='container'>
-        <article className='prose mx-auto'>
-          {router.isFallback ? <p>alert</p> : post.title}
-        </article>
+        <article className='prose mx-auto'>{post.fields.title}</article>
       </div>
     </section>
   );
 };
 
-export const getStaticProps = async ({ params, preview = false }: any) => {
+export const getStaticProps: GetStaticProps<PostProps> = async ({
+  params,
+  preview = false,
+}: any) => {
   const cfClient = preview ? previewClient : client;
 
   const { slug } = params;
@@ -27,22 +38,19 @@ export const getStaticProps = async ({ params, preview = false }: any) => {
 
   if (!response?.items?.length) {
     return {
-      redirect: {
-        destination: '/posts',
-        permanent: false,
-      },
+      notFound: true,
     };
   }
 
   return {
     props: {
-      post: response?.items?.[0],
+      post: response.items[0],
       preview,
     },
   };
 };
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const response = await client.getEntries({ content_type: 'post' });
   const paths = response.items.map((item: any) => ({
     params: { slug: item.fields.slug },
