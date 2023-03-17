8b28580e3e3857fd803e7d2aa57f4f1fca57dd18
IgorSwatowski <igorswatowski@gmail.com>
2023-03-17 19:57:26 +0100

    Fix

--- src/components/RichText.tsx
+++ src/components/RichText.tsx
@@ -5,7 +5,16 @@ import React from 'react';
 
 const options = {
   renderMark: {
-    [MARKS.CODE]: text => {
+    [MARKS.CODE]: (
+      text:
+        | string
+        | number
+        | boolean
+        | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+        | React.ReactFragment
+        | React.ReactPortal
+        | Iterable<React.ReactNode>,
+    ) => {
       return (
         <pre>
           <code>{text}</code>
@@ -14,10 +23,18 @@ const options = {
     },
   },
   renderNode: {
-    [BLOCKS.PARAGRAPH]: (node, children) => {
+    [BLOCKS.PARAGRAPH]: (
+      node: { content: any[] },
+      children:
+        | string
+        | number
+        | boolean
+        | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+        | React.ReactFragment,
+    ) => {
       if (
-        node.content.find(item =>
-          item.marks?.find(mark => mark.type === 'code'),
+        node.content.find((item: { marks: any[] }) =>
+          item.marks?.find((mark: { type: string }) => mark.type === 'code'),
         )
       ) {
         return (
@@ -32,7 +49,115 @@ const options = {
       return <p>{children}</p>;
     },
 
-    [INLINES.ENTRY_HYPERLINK]: node => {
+    [INLINES.ENTRY_HYPERLINK]: (node: {
+      data: {
+        target: {
+          sys: { contentType: { sys: { id: string } } };
+          fields: {
+            slug: any;
+            title:
+              | string
+              | number
+              | boolean
+              | React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                >
+              | React.ReactFragment
+              | React.ReactPortal
+              | (string &
+                  React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >)
+              | (string & React.ReactFragment)
+              | (string & React.ReactPortal)
+              | (number &
+                  React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >)
+              | (number & React.ReactFragment)
+              | (number & React.ReactPortal)
+              | (false &
+                  React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >)
+              | (false & React.ReactFragment)
+              | (false & React.ReactPortal)
+              | (true &
+                  React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >)
+              | (true & React.ReactFragment)
+              | (true & React.ReactPortal)
+              | (React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                > &
+                  string)
+              | (React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                > &
+                  number)
+              | (React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                > &
+                  false)
+              | (React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                > &
+                  true)
+              | (React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                > &
+                  React.ReactFragment)
+              | (React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                > &
+                  React.ReactPortal)
+              | (React.ReactFragment & string)
+              | (React.ReactFragment & number)
+              | (React.ReactFragment & false)
+              | (React.ReactFragment & true)
+              | (React.ReactFragment &
+                  React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >)
+              | (React.ReactFragment & React.ReactPortal)
+              | (React.ReactPortal & string)
+              | (React.ReactPortal & number)
+              | (React.ReactPortal & false)
+              | (React.ReactPortal & true)
+              | (React.ReactPortal &
+                  React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >)
+              | (React.ReactPortal & React.ReactFragment)
+              | (Iterable<React.ReactNode> & string)
+              | (Iterable<React.ReactNode> & number)
+              | (Iterable<React.ReactNode> & false)
+              | (Iterable<React.ReactNode> & true)
+              | (Iterable<React.ReactNode> &
+                  React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >)
+              | (Iterable<React.ReactNode> & React.ReactFragment)
+              | (Iterable<React.ReactNode> & React.ReactPortal);
+          };
+        };
+      };
+    }) => {
       if (node.data.target.sys.contentType.sys.id === 'post') {
         return (
           <Link href={`/posts/${node.data.target.fields.slug}`}>
@@ -42,8 +167,10 @@ const options = {
       }
     },
 
-    [INLINES.HYPERLINK]: node => {
-      const text = node.content.find(item => item.nodeType === 'text')?.value;
+    [INLINES.HYPERLINK]: (node: { content: any[]; data: { uri: string } }) => {
+      const text = node.content.find(
+        (item: { nodeType: string }) => item.nodeType === 'text',
+      )?.value;
       return (
         <a href={node.data.uri} target='_blank' rel='noopener noreferrer'>
           {text}
@@ -51,7 +178,14 @@ const options = {
       );
     },
 
-    [BLOCKS.EMBEDDED_ENTRY]: node => {
+    [BLOCKS.EMBEDDED_ENTRY]: (node: {
+      data: {
+        target: {
+          sys: { contentType: { sys: { id: string } } };
+          fields: { embedUrl: string; title: string };
+        };
+      };
+    }) => {
       if (node.data.target.sys.contentType.sys.id === 'videoEmbed') {
         return (
           <iframe
